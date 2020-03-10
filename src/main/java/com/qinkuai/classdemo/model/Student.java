4921fb9aca8605376f311785994bcd1ec0a94ec9
QinKuai <2609935221@qq.com>
2020-03-10 12:20:07 +0800

    完成数据库的设计&model层的编码

--- src/main/java/com/qinkuai/classdemo/model/Student.java
+++ src/main/java/com/qinkuai/classdemo/model/Student.java
@@ -1,17 +1,40 @@
 package com.qinkuai.classdemo.model;
 
+import java.util.Objects;
+
 import lombok.Data;
 
 @Data
 public class Student {
 	// 学生ID
 	private String id;
-	// 学生姓名
-	private String name;
+	// 学生姓
+	private String firstName;
+	// 学生名
+	private String lastName;
 	// 学生性别
 	private String sex;
 	// 学生班级名
 	private String className;
 	// 学生年级
 	private String grade;
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Student)) {
+			return false;
+		}
+		Student other = (Student) obj;
+		return Objects.equals(id, other.id);
+	}
 }
